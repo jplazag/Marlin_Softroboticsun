5124a145d70929c22a080ac51f3079e2eb18b577
jplazag <31490004+jplazag@users.noreply.github.com>
2021-02-02 10:46:28 -0500

    Configuración del espacio de trabajo y orientación de los ejes.
    Volumen de impresión e inversión del sentido en eje Y

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -831,7 +831,7 @@
 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
 #define INVERT_X_DIR false
-#define INVERT_Y_DIR true
+#define INVERT_Y_DIR false
 #define INVERT_Z_DIR false
 
 // @section extruder
@@ -861,8 +861,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 200
-#define Y_BED_SIZE 200
+#define X_BED_SIZE 215
+#define Y_BED_SIZE 215
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -870,7 +870,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 200
+#define Z_MAX_POS 280
 
 /**
  * Software Endstops
